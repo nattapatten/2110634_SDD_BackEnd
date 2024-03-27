ac5209097761a7fdce4fc1d2244ef77eaf9c9a13
Nattapat Pitchayanon <nattapart_ten@hotmail.com>
2024-03-27 21:27:18 +0700

    Initial Project3

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(cookieParser());
 
 
 
+const auth = require('./routes/auth');
+
+
+
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, ' mode on port ', PORT));
 
