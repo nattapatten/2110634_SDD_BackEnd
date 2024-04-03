1ca7115235fa2e8b0f1fa90a4f2aaecf0827a0f2
daniel-boon <61857025+daniel-boon@users.noreply.github.com>
2024-04-04 00:52:20 +0700

    adjust role and authen

--- routes/auth.js
+++ routes/auth.js
@@ -9,17 +9,17 @@ const {protect, authorize} = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
-// router.get('/users', protect, authorize('admin'), getUsers);
+router.get('/users', protect, authorize('admin'), getUsers);
 router.put('/users/:id', protect, authorize('admin'), updateUser);
-// router.delete('/users/:id', protect, authorize('admin'), deleteUser);
+router.delete('/users/:id', protect, authorize('admin'), deleteUser);
 router.post('/verify-otp', verifyOtp);
 
 
 // router.post('/register', register);
 // router.post('/login', login);
 // router.get('/me',  getMe);
-router.get('/users',   getUsers);
+// router.get('/users',   getUsers);
 // router.put('/users/:id', updateUser);
-router.delete('/users/:id',  deleteUser);
+// router.delete('/users/:id',  deleteUser);
 // router.post('/verify-otp', verifyOtp);
 module.exports = router;
