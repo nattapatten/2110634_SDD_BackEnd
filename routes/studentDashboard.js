b1778a80b5828988d420d02b3244eb562f007f4a
Nattapat Pitchayanon <nattapatten@gmail.com>
2024-04-22 18:57:28 +0700

    commt

--- routes/studentDashboard.js
+++ routes/studentDashboard.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { GetStudentDashboardData } = require('../controllers/studentDashboard');
+const { GetStudentDashboardData  , getStudentDashboardCourse} = require('../controllers/studentDashboard');
 
 // Route to get a student's courses by studentID
 router.get('/:studentID/dashboard', GetStudentDashboardData);
+
+// router.get('/:studentID/dashboard', getStudentDashboardCourse);
 
 module.exports = router;
